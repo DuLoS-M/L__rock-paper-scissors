5d41a69e53701463b9a3d2f7e572e79b4ffd4dbb
DuLoS-M <e_craud@hotmail.com>
2022-07-19 15:55:13 -0600

    added semicolons...

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -50,23 +50,20 @@ function playRound(playerSelection, computerSelection){
 function game() {
     let playerScore = 0;
     let computerScore = 0;
-    
+
 
     for (let i = 0; i < 5; i++) { 
         let playerMove = prompt('Choose a move');
         let roundResult = playRound(playerMove, getComputerChoice());
 
         if (roundResult.startsWith('You win!')) {
-            playerScore++ 
+            playerScore++;
         } else if (roundResult.startsWith('You lose!')) {
-            computerScore++
+            computerScore++;
         }
 
-        console.log(`${roundResult} \n --- \n The score is: \n You: ${playerScore} | CPU ${computerScore}`)
-
-
-
+        console.log(`${roundResult} \n --- \n The score is: \n You: ${playerScore} | CPU ${computerScore}`);
     }
 }
 
-game()
+game();
